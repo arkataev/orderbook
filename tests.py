156068e7cd00649d098057914dadfeceb92dc546
akataev <akataev@joincalibrate.com>
2021-11-24 21:35:22 +0300

    refactored get max price with iterative aproach

--- tests.py
+++ tests.py
@@ -1,57 +1,67 @@
-from dataclasses import dataclass
 import heapq
+from dataclasses import dataclass, field
 
 
-@dataclass
+@dataclass(order=True)
 class Order:
-    uid: int
-    price: float
+    uid: int = field(compare=False)
+    _price: float
+
+    def __post_init__(self):
+        # This is done to allow using this object in a min-heap as it was max-heap
+        self._price = -self._price
+
+    @property
+    def price(self):
+        # Not to confuse price usage, this should be reversed back to positive value
+        return -self._price
 
 
 class OrderBook:
     def __init__(self):
         self.init_timestamp = self.current_timestamp = 0
         self.twmp = 0.0
-        self.removed = {}
+        self.removed = set()
         self.pq = []
-        self.size = 0
 
     @property
     def twamp(self):
         """Get Time-Weighted Average Maximum Price"""
         return self.twmp / (self.current_timestamp - self.init_timestamp)
 
-    @property
-    def current_max_price(self) -> float:
-        if not self.pq:
+    def get_max_price(self) -> float:
+        while self.pq:
+            # if order was removed we can ignore as it is no longer the maximum value
+            order = self.pq[0]
+            if order.uid in self.removed:
+                heapq.heappop(self.pq)
+                self.removed.remove(order.uid)
+            else:
+                break
+        else:
+            # all orders removed or not yet added
             return 0.0
 
-        price, order_uid = self.pq[0] if self.pq else 0.0
-
-        if order_uid in self.removed:
-            price, order_uid = heapq.heappop(self.pq)
-            del self.removed[order_uid]
-            return self.current_max_price
-
-        return -self.pq[0][0] if self.pq else 0.0
+        return order.price
 
     def add(self, timestamp: int, order: Order):
         if not self.init_timestamp:
             self.init_timestamp = timestamp
 
-        current_max = self.current_max_price
+        current_max = self.get_max_price()
 
         if current_max != order.price:
             self.twmp += (timestamp - self.current_timestamp) * current_max
             self.current_timestamp = timestamp
 
-        heapq.heappush(self.pq, (-order.price, order.uid))
+        heapq.heappush(self.pq, order)
 
     def remove(self, timestamp: int, order_id: int):
-        current_max = self.current_max_price
-        self.removed[order_id] = 1
+        current_max = self.get_max_price()
 
-        new_max = self.current_max_price
+        self.removed.add(order_id)
+
+        new_max = self.get_max_price()
 
         if current_max != new_max:
             self.twmp += (timestamp - self.current_timestamp) * current_max
@@ -64,7 +74,7 @@ def test_add_order_with_same_price():
     order_book.add(2000, Order(101, 10.0))
 
     assert order_book.current_timestamp == order_book.init_timestamp == 1000
-    assert order_book.current_max_price == 10.0
+    assert order_book.get_max_price() == 10.0
 
 
 def test_add_order():
@@ -73,7 +83,7 @@ def test_add_order():
     order_book.add(2000, Order(101, 11.0))
 
     assert order_book.current_timestamp == 2000
-    assert order_book.current_max_price == 11.0
+    assert order_book.get_max_price() == 11.0
 
 
 def test_delete_order():
@@ -81,10 +91,10 @@ def test_delete_order():
     order_book.add(1000, Order(100, 10.0))
     order_book.add(1000, Order(101, 13.0))
 
-    assert order_book.current_max_price == 13.0
+    assert order_book.get_max_price() == 13.0
 
     order_book.remove(2000, 101)
-    assert order_book.current_max_price == 10.0
+    assert order_book.get_max_price() == 10.0
 
 
 def test_twamp():
